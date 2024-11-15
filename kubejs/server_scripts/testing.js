f5daa0dd45fbcd6a9cb27288ac0178318e666fca
Niko <niko.parssinen@tuni.fi>
2024-11-15 09:46:17 +0200

    progress

--- kubejs/server_scripts/testing.js
+++ kubejs/server_scripts/testing.js
@@ -30,4 +30,44 @@ ServerEvents.recipes(event => {
         B: 'minecraft:dirt'           
       }
   )
+
+  //BASIC POWER FLOWER
+  event.remove({ output: 'projectexpansion:basic_power_flower' })
+  event.recipes.extendedcrafting.shaped_table(
+    'projectexpansion:basic_power_flower', [
+        'EDADE',
+        'DACAD',
+        'ACBCA',
+        'DACAD',
+        'EDADE'
+    ], {
+      A: 'projectexpansion:basic_compressed_collector', 
+      B: 'projectexpansion:basic_emc_link',
+      C: 'projectexpansion:basic_relay',
+      D: 'projecte:aeternalis_fuel',
+      E: 'mysticalagriculture:inferium_essence'
+    }
+  )
+
+  //DARK POWER FLOWER
+  event.remove({ output: 'projectexpansion:dark_power_flower' })
+  event.recipes.extendedcrafting.shaped_table(
+    'projectexpansion:dark_power_flower', [
+        'FFDADFF',
+        'FDACADF',
+        'DAECEAD',
+        'ACCBCCA',
+        'DAECEAD',
+        'FDACADF',
+        'FFDADFF'
+    ], {
+      A: 'projectexpansion:dark_compressed_collector', 
+      B: 'projectexpansion:dark_emc_link',
+      C: 'projectexpansion:dark_relay',
+      D: 'projecte:dark_matter',
+      E: 'projectexpansion:basic_power_flower',
+      F: 'mysticalagriculture:inferium_essence'
+    }
+  )
+
 })
